1ac4f25f57043cadaf1645172a3a082e6e006d45
seonglae <seonglae.c@stryx.co.kr>
2019-12-19 17:59:59 +0900

    🦪bash file

--- script/vm/start-vms.bat
+++ script/vm/start-vms.bat
@@ -2,3 +2,5 @@
 "C:\Program Files\Oracle\VirtualBox\VBoxManage.exe" startvm Ansible-Node1 --type headless
 "C:\Program Files\Oracle\VirtualBox\VBoxManage.exe" startvm Ansible-Node2 --type headless
 "C:\Program Files\Oracle\VirtualBox\VBoxManage.exe" startvm Ansible-Node3 --type headless
+"C:\Program Files\Oracle\VirtualBox\VBoxManage.exe" startvm Ansible-Node4 --type headless
+"C:\Program Files\Oracle\VirtualBox\VBoxManage.exe" startvm Ansible-Node5 --type headless
