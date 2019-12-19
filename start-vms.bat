9cde39d07b0adcfb8a86e7659764be0a8f750f29
seonglae <seonglae.c@stryx.co.kr>
2019-12-19 15:15:18 +0900

    🍟first commit

---
+++ start-vms.bat
@@ -0,0 +1,4 @@
+"C:\Program Files\Oracle\VirtualBox\VBoxManage.exe" startvm Ansible-Server --type headless
+"C:\Program Files\Oracle\VirtualBox\VBoxManage.exe" startvm Ansible-Node1 --type headless
+"C:\Program Files\Oracle\VirtualBox\VBoxManage.exe" startvm Ansible-Node2 --type headless
+"C:\Program Files\Oracle\VirtualBox\VBoxManage.exe" startvm Ansible-Node3 --type headless
