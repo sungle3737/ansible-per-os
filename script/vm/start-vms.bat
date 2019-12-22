cd0c5039f717e6fbf1a6a6fd7f5d102f1e48a075
Seonglae <sungle3737@gmail.com>
2019-12-22 19:10:21 +0900

    🍜most and temp commit

--- script/vm/start-vms.bat
+++ script/vm/start-vms.bat
@@ -4,3 +4,11 @@
 "C:\Program Files\Oracle\VirtualBox\VBoxManage.exe" startvm Ansible-Node3 --type headless
 "C:\Program Files\Oracle\VirtualBox\VBoxManage.exe" startvm Ansible-Node4 --type headless
 "C:\Program Files\Oracle\VirtualBox\VBoxManage.exe" startvm Ansible-Node5 --type headless
+
+
+"C:\Program Files\Oracle\VirtualBox\VBoxManage.exe" controlvm  Ansible-Server reset
+"C:\Program Files\Oracle\VirtualBox\VBoxManage.exe" controlvm  Ansible-Node1 reset
+"C:\Program Files\Oracle\VirtualBox\VBoxManage.exe" controlvm  Ansible-Node2 reset
+"C:\Program Files\Oracle\VirtualBox\VBoxManage.exe" controlvm  Ansible-Node3 reset
+"C:\Program Files\Oracle\VirtualBox\VBoxManage.exe" controlvm  Ansible-Node4 reset
+"C:\Program Files\Oracle\VirtualBox\VBoxManage.exe" controlvm  Ansible-Node5 reset
