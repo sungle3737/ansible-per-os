bbcf005ea7cfbeef7c23112c2966e4ae0b64b326
Seonglae <sungle3737@gmail.com>
2019-12-23 15:15:27 +0900

    gi

--- script/cent/cent42-v1.sh
+++ script/cent/cent42-v1.sh
@@ -8,30 +8,5 @@
 # Short-Description: test
 # Description:       test daemon
 ### END INIT INFO
-if [ "$UID" -ne 0 ]; then
-    echo "You must be root to run this script"
-    exit 1
-fi
-
-sudo su
-
-#apache
-service apache2 start
-
-#open web
-
-
-#mount
-mkdir /mnt/nas/smms_output -p
-yum install -y cifs-utils
 mount -t cifs //10.0.0.148/smms_output /mnt/nas/smms_output -o user=Sln_Server_Account,pass=Tmxmfltm\!234,rw,vers=1.0
-
-#set svr root
-SVR_ROOT=/mnt/nas/smms_output
-
-# install node
-# install pm2 global
-
-#start pm2
-pm2 serve pm2 serve $SVR_ROOT 58022 --name "nassvr_code42_1"
 
