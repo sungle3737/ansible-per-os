f42e4bb34b4aa3e5e288007f7aa51aae6da22b31
Seonglae <sungle3737@gmail.com>
2019-12-26 14:01:49 +0900

    easy

--- start-vms.bat
+++ start-vms.bat
@@ -5,6 +5,7 @@
 "C:\Program Files\Oracle\VirtualBox\VBoxManage.exe" controlvm  Ansible-Node3 poweroff soft
 "C:\Program Files\Oracle\VirtualBox\VBoxManage.exe" controlvm  Ansible-Node4 poweroff soft
 "C:\Program Files\Oracle\VirtualBox\VBoxManage.exe" controlvm  Ansible-Node5 poweroff soft
+"C:\Program Files\Oracle\VirtualBox\VBoxManage.exe" controlvm  test poweroff soft
 
 "C:\Program Files\Oracle\VirtualBox\VBoxManage.exe" controlvm  Ansible-Server reset
 "C:\Program Files\Oracle\VirtualBox\VBoxManage.exe" controlvm  Ansible-Node1 reset
@@ -12,6 +13,7 @@
 "C:\Program Files\Oracle\VirtualBox\VBoxManage.exe" controlvm  Ansible-Node3 reset
 "C:\Program Files\Oracle\VirtualBox\VBoxManage.exe" controlvm  Ansible-Node4 reset
 "C:\Program Files\Oracle\VirtualBox\VBoxManage.exe" controlvm  Ansible-Node5 reset
+"C:\Program Files\Oracle\VirtualBox\VBoxManage.exe" controlvm  test reset
 
 "C:\Program Files\Oracle\VirtualBox\VBoxManage.exe" startvm Ansible-Server --type headless
 "C:\Program Files\Oracle\VirtualBox\VBoxManage.exe" startvm Ansible-Node1 --type headless
@@ -19,3 +21,4 @@
 "C:\Program Files\Oracle\VirtualBox\VBoxManage.exe" startvm Ansible-Node3 --type headless
 "C:\Program Files\Oracle\VirtualBox\VBoxManage.exe" startvm Ansible-Node4 --type headless
 "C:\Program Files\Oracle\VirtualBox\VBoxManage.exe" startvm Ansible-Node5 --type headless
+"C:\Program Files\Oracle\VirtualBox\VBoxManage.exe" startvm test --type headless
