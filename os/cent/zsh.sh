8cff7109046675126e9836ded84814e0d7261f11
Seonglae <sungle3737@gmail.com>
2020-01-07 16:33:53 +0900

    rc everywherer

--- os/cent/zsh.sh
+++ os/cent/zsh.sh
@@ -1,4 +1,8 @@
 sudo yum -y install zsh
 chsh -s /bin/zsh
 zsh
+2
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
+echo 'export TERM=xterm-256color' >> ~/.zshrc
+echo 'ZSH_THEME=powerlevel10k/powerlevel10k' >> ~/.zshrc
